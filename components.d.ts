f8e832e059db1dbd09156ea6c18b0cec95559d97
Rider君 <11827413+mr-rider@user.noreply.gitee.com>
2024-04-24 21:45:32 +0800

    update

--- components.d.ts
+++ components.d.ts
@@ -35,6 +35,8 @@ declare module 'vue' {
     ElOption: typeof import('element-plus/es')['ElOption']
     ElPageHeader: typeof import('element-plus/es')['ElPageHeader']
     ElPagination: typeof import('element-plus/es')['ElPagination']
+    ElRadio: typeof import('element-plus/es')['ElRadio']
+    ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElStep: typeof import('element-plus/es')['ElStep']

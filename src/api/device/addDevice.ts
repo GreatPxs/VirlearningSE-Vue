1221879f268cf97b9383e3895601bd9697ce2743
Rider君 <11827413+mr-rider@user.noreply.gitee.com>
2024-04-25 18:59:51 +0800

    update

--- src/api/device/addDevice.ts
+++ src/api/device/addDevice.ts
@@ -1,8 +1,19 @@
 import request from '@/utils/request.js'
 
 export const addDevice = (form) => {
+  console.log(
+    `/device/add?id=0&isdeleted=0&name=` +
+      form.name +
+      `&feature=` +
+      form.feature +
+      `&photo=` +
+      form.photo +
+      `&video=` +
+      form.video +
+      `&dep`
+  )
   return request({
-    method: 'GET',
+    method: 'POST',
 
     url:
       `/device/add?id=0&isdeleted=0&name=` +
